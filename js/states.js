486b8510c32d555cc888a59f396c133cf54d4256
Ernesto Garcia Valenzuela <0322103721@ut-tijuana.edu.mx>
2025-09-10 20:36:45 -0800

    Archivo states
    Son las probabilidades de subida y bajada de ritmo cardiaco en los eventos

---
+++ js/states.js
@@ -0,0 +1,8 @@
+export const states = {
+reposo: {name: "Reposo", probabilities: {same: 50, up: 25, down: 25}},
+dormir: {name: "Dormir", probabilities: {same: 70, up: 20, down: 10}},
+despertar: {name: "Despertar", probabilities: {same: 40, up: 40, down: 20}},
+caminar: {name: "Caminar", probabilities: {same: 50, up: 30, down: 20}},
+correr: {name: "Correr", probabilities: {same: 70, up: 20, down: 10}},
+susto: {name: "Susto", probabilities: {same: 0, up: 90, down: 10}},
+};
